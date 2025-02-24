93bd61cfbbdc35166ea637ac441bf79bc1d9ff0f
Reyhaneh-hm <reyhaneh3784@gmail.com>
2025-02-25 01:50:41 +0330

    Add responsive landing page

--- src/js/pages/help/help.js
+++ src/js/pages/help/help.js
@@ -1,75 +1,25 @@
 import '../../../scss/components/pages/help/help.scss';
 
-/*--------------------------------------------------------*/
-// document.querySelectorAll(".list a").forEach(link => {
-//     link.addEventListener("click", function (e) {
-//         e.preventDefault();  
+/*--------------------Add Active to item list------------------*/
+document.querySelectorAll(".list a").forEach(link => {
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
 
-//         document.querySelectorAll(".list a").forEach(item => {
-//             item.classList.remove("active");
-//         });
-
-//         this.classList.add("active");
-
-//         let target = document.querySelector(this.getAttribute("href"));
-//         if (target) {
-//             window.scrollTo({
-//                 top: target.offsetTop - 20,
-//                 behavior: "smooth"
-//             });
-//         }
-//     });
-// });
-document.addEventListener("DOMContentLoaded", function () {
-    const links = document.querySelectorAll(".list a");
-    const sections = document.querySelectorAll(".content");
-
-    // ✅ تابع برای حذف و اضافه کردن کلاس active
-    function activateLink(targetId) {
-        links.forEach(link => {
-            link.classList.remove("active");
-            if (link.getAttribute("href") === `#${targetId}`) {
-                link.classList.add("active");
-            }
+        document.querySelectorAll(".list a").forEach(item => {
+            item.classList.remove("active");
         });
-    }
 
-    // ✅ مدیریت کلیک روی آیتم‌ها
-    links.forEach(link => {
-        link.addEventListener("click", function (e) {
-            e.preventDefault(); // جلوگیری از رفتار پیش‌فرض
+        this.classList.add("active");
 
-            // حذف active از همه لینک‌ها و اضافه کردن به مورد کلیک‌شده
-            links.forEach(item => item.classList.remove("active"));
-            this.classList.add("active");
-
-            let target = document.querySelector(this.getAttribute("href"));
-            if (target) {
-                window.scrollTo({
-                    top: target.offsetTop - 20,
-                    behavior: "smooth"
-                });
-            }
-        });
+        let target = document.querySelector(this.getAttribute("href"));
+        if (target) {
+            window.scrollTo({
+                top: target.offsetTop - 20,
+                behavior: "smooth"
+            });
+        }
     });
-
-    // ✅ مدیریت اسکرول و تشخیص بخش در حال نمایش
-    function handleScroll() {
-        let scrollPosition = window.scrollY + window.innerHeight / 3; // نقطه‌ای که می‌خوایم چک کنیم
-
-        sections.forEach(section => {
-            let sectionTop = section.offsetTop;
-            let sectionHeight = section.offsetHeight;
-
-            if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
-                activateLink(section.id);
-            }
-        });
-    }
-
-    window.addEventListener("scroll", handleScroll);
 });
-
 
 /*-----------------------show search-box------------------------*/
 document.addEventListener("DOMContentLoaded", () => {
--- src/js/pages/landing/landing.js
+++ src/js/pages/landing/landing.js
@@ -93,96 +93,113 @@ new Chart(abc, {
     }
 });
 
-/*-----------------------Chart month-------------------------*/
-const asd = document.getElementById('waveChart').getContext('2d');
-
-const labels = ["فروردین", "اردیبهشت", "خرداد", "تیر", "مرداد", "شهریور", "مهر", "آبان", "آذر", "دی", "بهمن", "اسفند"];
-
-const dataPoints = [50, 50, 70, 40, 90, 15, 50, 30, 60, 25];
-
-const maxValue = Math.max(...dataPoints);
-const minValue = Math.min(...dataPoints);
-const maxIndex = dataPoints.indexOf(maxValue);
-const minIndex = dataPoints.indexOf(minValue);
-
-const gradient = asd.createLinearGradient(0, 0, 0, 400);
-gradient.addColorStop(0, '#AEC8EF');
-gradient.addColorStop(0.5, '#ffffff');
-
-new Chart(asd, {
-    type: 'line',
-    data: {
-        labels: labels,
-        datasets: [{
-            label: "",
-            data: dataPoints,
-            borderColor: "#0B6FCC",
-            borderWidth: 2,
-            backgroundColor: gradient,
-            fill: true,
-            pointRadius: 0,
-            pointBackgroundColor: "#0B6FCC",
-            tension: 0.5
-        }]
-    },
-    options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-            x: {
-                grid: {
-                    display: false,
-                    drawBorder: false,
-                    borderColor: 'transparent'
-                },
-                border: {
-                    display: false
-                },
-                ticks: {
-                    display: false
-                }
-            },
-            y: {
-                beginAtZero: false,
-                grid: {
-                    display: false,
-                    drawBorder: false,
-                    borderColor: 'transparent'
-                },
-                border: {
-                    display: false
-                },
-                ticks: {
-                    display: false
-                }
-            }
-        },
-        plugins: {
-            legend: {
-                display: false
-            },
-            tooltip: {
-                enabled: false,
-                backgroundColor: 'rgba(0,0,0,0.8)',
-                titleFont: { size: 1420, weight: 'bold' },
-                bodyFont: { size: 12 }
-            }
-        }
-    }
-});
-
-document.getElementById('maxLabel').style.left = `${(maxIndex / labels.length) * 100}%`;
-document.getElementById('maxLabel').textContent = maxValue;
-
-document.getElementById('minLabel').style.left = `${(minIndex / labels.length) * 100}%`;
-document.getElementById('minLabel').textContent = minValue;
-
-/*----------------Dark & Light-----------------*/
+/*-----------Chart month and Dark & Light-----------------*/
 
 document.addEventListener("DOMContentLoaded", function () {
+    const asd = document.getElementById('waveChart').getContext('2d');
+
+    const labels = ["فروردین", "اردیبهشت", "خرداد", "تیر", "مرداد", "شهریور", "مهر", "آبان", "آذر", "دی", "بهمن", "اسفند"];
+    const dataPoints = [50, 50, 70, 40, 90, 15, 50, 30, 60, 25];
+
+    const maxValue = Math.max(...dataPoints);
+    const minValue = Math.min(...dataPoints);
+    const maxIndex = dataPoints.indexOf(maxValue);
+    const minIndex = dataPoints.indexOf(minValue);
+
+    function setGradient() {
+        const gradient = asd.createLinearGradient(0, 0, 0, 400);
+        if (document.documentElement.classList.contains('dark')) {
+            gradient.addColorStop(1, 'rgba(22, 22, 22, 0)');
+            gradient.addColorStop(0, 'rgba(26, 40, 61, 0.5)');
+        } else {
+            gradient.addColorStop(0, '#AEC8EF');
+            gradient.addColorStop(0.5, '#ffffff');
+        }
+        return gradient;
+    }
+
+    const gradient = setGradient();
+
+    const chart = new Chart(asd, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: "",
+                data: dataPoints,
+                borderColor: "#0B6FCC",
+                borderWidth: 2,
+                backgroundColor: gradient,
+                fill: true,
+                pointRadius: 0,
+                pointBackgroundColor: "#0B6FCC",
+                tension: 0.5
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                x: {
+                    grid: {
+                        display: false,
+                        drawBorder: false,
+                        borderColor: 'transparent'
+                    },
+                    border: {
+                        display: false
+                    },
+                    ticks: {
+                        display: false
+                    }
+                },
+                y: {
+                    beginAtZero: false,
+                    grid: {
+                        display: false,
+                        drawBorder: false,
+                        borderColor: 'transparent'
+                    },
+                    border: {
+                        display: false
+                    },
+                    ticks: {
+                        display: false
+                    }
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false
+                },
+                tooltip: {
+                    enabled: false,
+                    backgroundColor: 'rgba(0,0,0,0.8)',
+                    titleFont: { size: 1420, weight: 'bold' },
+                    bodyFont: { size: 12 }
+                }
+            }
+        }
+    });
+
+    document.getElementById('maxLabel').style.left = `${(maxIndex / labels.length) * 100}%`;
+    document.getElementById('maxLabel').textContent = maxValue;
+
+    document.getElementById('minLabel').style.left = `${(minIndex / labels.length) * 100}%`;
+    document.getElementById('minLabel').textContent = minValue;
+
     const switchInput = document.querySelector(".switch .input");
     switchInput.addEventListener("change", function () {
         document.documentElement.classList.toggle("dark");
+        chart.data.datasets[0].backgroundColor = setGradient();
+        chart.update();
     });
-});
 
+    const switchInput2 = document.querySelector(".switch2 .input");
+    switchInput2.addEventListener("change", function () {
+        document.documentElement.classList.toggle("dark");
+        chart.data.datasets[0].backgroundColor = setGradient();
+        chart.update();
+    });
+
+});
